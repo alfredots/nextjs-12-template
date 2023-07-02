92b571725f390f6ff01f20a1488147c7e12bc328
Alfredo Tito <tito.alfred@hotmail.com>
2023-07-02 18:24:57 -0300

    feat: add tailwind prettier

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,12 @@
-import * as S from './styles'
-
 export const HomeView = () => {
   return (
-    <div className={S.mainClass}>
-      TWD<span className={S.spanClass}>NEXT</span>
+    <div
+      className="flex h-screen
+        w-screen
+        items-center justify-center
+      bg-zinc-800 text-9xl text-white"
+    >
+      TWD<span className="text-blue-700">NEXT</span>
     </div>
   )
 }
